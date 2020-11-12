52b2c9d907cb91135ace10c80cbad2c2630116b5
Tancre <outisisout@gmail.com>
2020-11-12 13:06:51 +0100

    first commit

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+
+function theme_files() {
+  wp_enqueue_script('main-theme-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true );
+  wp_enqueue_style('custom-google-font', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+  wp_enqueue_style('font-awsome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+  wp_enqueue_style('theme_main_style', get_stylesheet_uri(), NULL, microtime());
+}
+
+add_action('wp_enqueue_scripts', 'theme_files');
+
+function theme_features() {
+  add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme', 'theme_features');
